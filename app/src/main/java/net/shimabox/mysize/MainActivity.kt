1ec8a0fbc0a1ef2411eb7c72fc51f673139886c4
shimabox <shimabox@gmail.com>
2020-12-25 23:36:08 +0900

    :sparkles: 保存処理実装.

--- app/src/main/java/net/shimabox/mysize/MainActivity.kt
+++ app/src/main/java/net/shimabox/mysize/MainActivity.kt
@@ -2,10 +2,37 @@ package net.shimabox.mysize
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.preference.PreferenceManager
+import net.shimabox.mysize.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        val editNeck = pref.getString("NECK", "")
+        val editSleeve = pref.getString("SLEEVE", "")
+        val editWaist = pref.getString("WAIST", "")
+        val editInseam = pref.getString("INSEAM", "")
+
+        binding.neck.setText(editNeck)
+        binding.sleeve.setText(editSleeve)
+        binding.waist.setText(editWaist)
+        binding.inseam.setText(editInseam)
+
+        binding.save.setOnClickListener { onSaveTapped() }
+    }
+
+    private fun onSaveTapped() {
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        val editor = pref.edit()
+        editor.putString("NECK", binding.neck.text.toString())
+              .putString("SLEEVE", binding.sleeve.text.toString())
+              .putString("WAIST", binding.waist.text.toString())
+              .putString("INSEAM", binding.inseam.text.toString())
+              .apply()
     }
 }
